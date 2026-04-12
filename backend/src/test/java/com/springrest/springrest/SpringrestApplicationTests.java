90778ce0c1cc5c9a97c931fb7105d23caca0dd9f
sneha-0523 <snehavarshney581@gmail.com>
2026-04-13 00:00:40 +0530

    fix tests using H2 database

--- backend/src/test/java/com/springrest/springrest/SpringrestApplicationTests.java
+++ backend/src/test/java/com/springrest/springrest/SpringrestApplicationTests.java
@@ -2,12 +2,14 @@ package com.springrest.springrest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringrestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
